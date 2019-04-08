d3c0fa6c4cdc91f15e74f60cd51cfda2939a2574
Jiwook Seo <spjo21@naver.com>
2019-04-08 15:31:25 +0900

    Add papago.py

---
+++ cooking/papago.py
@@ -0,0 +1,21 @@
+import os
+import urllib.request
+
+client_id = os.getenv('NAVER_ID')
+client_secret = os.getenv('NAVER_SECRET')
+
+
+def translate(text):
+    encText = urllib.parse.quote(text)
+    data = "source=ko&target=en&text=" + encText
+    url = "https://openapi.naver.com/v1/language/translate"
+    request = urllib.request.Request(url)
+    request.add_header("X-Naver-Client-Id", client_id)
+    request.add_header("X-Naver-Client-Secret", client_secret)
+    response = urllib.request.urlopen(request, data=data.encode("utf-8"))
+    code = response.getcode()
+    if code == 200:
+        res = response.read().decode('utf-8')
+        return True, res.split(',')[3].split(':')[2][1:-1]
+    else:
+        return False, None
