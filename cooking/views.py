e05ce1e3bf8c9f8794373a35543b2d5d94c627a7
ejolie <ejisvip@gmail.com>
2019-04-09 14:15:38 +0900

    Add variable_list in urls.py and views.py

--- cooking/views.py
+++ cooking/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
-
 def index(request):
     return render(request, 'cooking/index.html')
+
+def variable_list(request):
+    return render(request, 'cooking/show.html')
