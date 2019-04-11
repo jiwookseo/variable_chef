6b2c1d8c4fb63fee8592b1da0161817a928b9398
Jiwook Seo <spjo21@naver.com>
2019-04-11 13:03:43 +0900

    Update templates, add variables component and update index

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'store'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('search/', views.index, name='search'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Word
 
 
 def index(request):
-    return render(request, 'store/index.html')
+    word = request.GET.get('word', None)
+    var_set = None
+    if word:
+        temp = Word.objects.filter(kr_word=word)
+        word = temp[0] if temp else Word.create(word)
+        var_set = word.variable_set.order_by('-hits')
+    return render(request, 'store/index.html', {'var_set': var_set})
