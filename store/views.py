3bd697b97900e3dd49817ef6161192aa0f2fb972
Jiwook Seo <spjo21@naver.com>
2019-04-15 16:44:08 +0900

    Update variables action, change template parameter variable_set to Word object

--- store/views.py
+++ store/views.py
@@ -4,14 +4,12 @@ from .models import Word
 
 def index(request):
     word = request.GET.get('word', None)
-    var_set = None
     if word:
         temp = Word.objects.filter(kr_word=word)
         word = temp[0] if temp else Word.create(word)
-        var_set = word.variable_set.all()
         # session 당 1번 제한, 모든 Variables 조회 수 증가
         # if not request.session.get(str(word.id) + 'hit', False):
         #     request.session[str(word.id) + 'hit'] = True
-        #     for var in var_set:
+        #     for var in word.variable_set.all():
         #         var.update_hits
-    return render(request, 'store/index.html', {'var_set': var_set})
+    return render(request, 'store/index.html', {'word': word})
