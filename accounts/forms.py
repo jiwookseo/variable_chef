cb7eed946e3f429e06192df6822b23bab63b0a77
Jiwook Seo <spjo21@naver.com>
2019-04-16 14:35:40 +0900

    Add accounts app

---
+++ accounts/forms.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth import get_user_model
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('password', 'email', 'username',)
