162e63d4bb6f843a08db8b846133c121af2a8984
Jiwook Seo <spjo21@naver.com>
2019-04-16 17:19:51 +0900

    Fix wrong typo

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -4,9 +4,5 @@ from . import views
 app_name = 'store'
 urlpatterns = [
     path('', views.index, name='index'),
-<<<<<<< HEAD:cooking/urls.py
-    path('list/', views.variable_list, name='variable_list')
-=======
     path('search/', views.index, name='search'),
->>>>>>> upstream/master:store/urls.py
 ]
