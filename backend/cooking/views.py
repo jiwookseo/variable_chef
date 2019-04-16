db728dcc6f226869ecd1b4703e92428c1df8c493
Jiwook Seo <spjo21@naver.com>
2019-04-16 17:24:37 +0900

    Delete overlapped files

--- backend/cooking/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'cooking/index.html')
-
-def variable_list(request):
-    return render(request, 'cooking/show.html')
