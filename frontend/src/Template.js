92b438a66d92d7dd06d799a490fe04a671c4408a
jiwookseo <jiwookseo.dev@gmail.com>
2019-10-16 02:45:05 +0900

    [MOD] Update API URL

--- frontend/src/Template.js
+++ frontend/src/Template.js
@@ -1,29 +1,32 @@
-import React, { Component, Fragment } from 'react';
-import { Grid } from 'semantic-ui-react';
-import axios from 'axios';
+import React, { Component, Fragment } from "react";
+import { Grid } from "semantic-ui-react";
+import axios from "axios";
 // import styled from 'styled-components';
 
-import Heading from './Heading';
-import SearchBar from './SearchBar';
-import WordList from './WordList';
+import Heading from "./Heading";
+import SearchBar from "./SearchBar";
+import WordList from "./WordList";
+
+const BASE_URL =
+  "http://ec2-15-164-231-243.ap-northeast-2.compute.amazonaws.com:8000";
 
 class Template extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      word: '',
-      lang: 'python',
+      word: "",
+      lang: "python",
       wordError: false,
       searchable: false,
-      variables: [],
+      variables: []
     };
   }
 
-  handleInputChange = (event) => {
+  handleInputChange = event => {
     this.setState({
       [event.target.name]: event.target.value
     });
-  }
+  };
 
   handleSubmit = event => {
     event.preventDefault();
@@ -31,51 +34,50 @@ class Template extends Component {
 
     // FIXME: Word validation
     const regExp = /[^가-힣\s]/g;
-    if (word === '' || regExp.test(word)) {
-      this.setState((prevState) => { 
-        return { wordError: prevState.wordError || true } 
+    if (word === "" || regExp.test(word)) {
+      this.setState(prevState => {
+        return { wordError: prevState.wordError || true };
       });
-      console.log('완전한 한글이 아니네요.. wordError', wordError);
-    } 
-    else {
-      this.setState((prevState) => { 
-        return { wordError: prevState.wordError && false } 
+      // console.log("완전한 한글이 아니네요.. wordError", wordError);
+    } else {
+      this.setState(prevState => {
+        return { wordError: prevState.wordError && false };
       });
-      console.log('좋아요.. wordError', wordError);
+      // console.log("좋아요.. wordError", wordError);
     }
 
     if (!wordError) {
-      this.setState((prevState) => {
-        return { searchable: prevState.searchable || true }
-      })
+      this.setState(prevState => {
+        return { searchable: prevState.searchable || true };
+      });
 
       // TODO - Search logic, Render WordList
-      axios.get(`http://127.0.0.1:8000/test/${word}`)
-      .then(response => {
-        this.setState({
-          variables: response.data.variables
+      axios
+        .get(`${BASE_URL}/test/${word}`)
+        .then(response => {
+          this.setState({
+            variables: response.data.variables
+          });
+          // console.log(this.state.variables);
         })
-        console.log(this.state.variables);
-      })
-      .catch(error => {
-        console.log(error);
+        .catch(error => {
+          // console.log(error);
+        });
+    } else {
+      this.setState(prevState => {
+        return { searchable: prevState.searchable && false };
       });
-    } 
-    else {
-      this.setState((prevState) => {
-        return { searchable: prevState.searchable && false }
-      })
     }
 
-    console.log('searchable', searchable);
-  }
+    // console.log("searchable", searchable);
+  };
 
   render() {
     return (
       <Fragment>
-        <Heading/>
+        <Heading />
         <Grid centered columns={2}>
-          <SearchBar 
+          <SearchBar
             word={this.state.word}
             lang={this.state.lang}
             wordError={this.state.wordError}
@@ -84,17 +86,11 @@ class Template extends Component {
           />
         </Grid>
         {/* FIXME - 검색 결과 반환 시 렌더링 */}
-        { this.state.searchable && 
-          <Grid 
-            centered 
-            columns={3}
-          >
-            <WordList 
-              variables={this.state.variables}
-              lang={this.state.lang}
-            />
+        {this.state.searchable && (
+          <Grid centered columns={3}>
+            <WordList variables={this.state.variables} lang={this.state.lang} />
           </Grid>
-        }
+        )}
       </Fragment>
     );
   }
