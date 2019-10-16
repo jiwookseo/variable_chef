0f00cf178d498c55b83b80ccbf40ae45fd58b64b
jiwookseo <jiwookseo.dev@gmail.com>
2019-10-16 11:03:08 +0900

    [FIX] Fix missing trailing slash in urls.py

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -20,14 +20,17 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('test/<str:q>', views.test, name='test'),
+    path('test/<str:q>/', views.test, name='test'),
     path('hits/', views.hits, name='hits'),
     path('search/', views.index, name='search'),
     path('api/word/', views.ListWord.as_view()),
     path('api/word/<int:pk>/', views.DetailWord.as_view()),
     path('api/variable/', views.ListVar.as_view()),
     path('api/variable/<int:pk>/', views.DetailVar.as_view()),
-    path('swagger<str:format>', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger<str:format>', schema_view.without_ui(
+        cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger',
+                                         cache_timeout=0), name='schema-swagger-ui'),
+    path('docs/', schema_view.with_ui('redoc',
+                                      cache_timeout=0), name='schema-redoc'),
 ]
